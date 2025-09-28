2f54b4822a47874d63dbb9d3c105ea8eb97d7637
William Lohrmann <will@hoxtonfarms.com>
2025-09-28 17:24:59 +0100

    Remove extras

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,13 +1,11 @@
 import type { Action, ThunkAction } from "@reduxjs/toolkit"
 import { combineSlices, configureStore } from "@reduxjs/toolkit"
 import { setupListeners } from "@reduxjs/toolkit/query"
-import { counterSlice } from "../features/counter/counterSlice"
-import { quotesApiSlice } from "../features/quotes/quotesApiSlice"
 import { channelsReducer } from "./channelsSlice"
 
 // `combineSlices` automatically combines the reducers using
 // their `reducerPath`s, therefore we no longer need to call `combineReducers`.
-const rootReducer = combineSlices(counterSlice, quotesApiSlice, {
+const rootReducer = combineSlices({
   channelsState: channelsReducer,
 })
 // Infer the `RootState` type from the root reducer
@@ -21,7 +19,7 @@ export const makeStore = (preloadedState?: Partial<RootState>) => {
     // Adding the api middleware enables caching, invalidation, polling,
     // and other useful features of `rtk-query`.
     middleware: getDefaultMiddleware => {
-      return getDefaultMiddleware().concat(quotesApiSlice.middleware)
+      return getDefaultMiddleware()
     },
     preloadedState,
   })
